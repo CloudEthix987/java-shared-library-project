be38562d5b52f725f3c24f78ee4b627707f650cc
CloudEthix987 <payaldkharat@coep.sveri.ac.in>
2026-09-17 23:39:57 +0530

    Update Hello.java

--- src/Hello.java
+++ src/Hello.java
@@ -1,5 +1,10 @@
 public class Hello {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+
         System.out.println("Hello from Simple Project!");
+
+        while (true) {
+            Thread.sleep(10000);
+        }
     }
 }
